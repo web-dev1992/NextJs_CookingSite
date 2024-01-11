69d3cab191271bedf87bf12be806ba1d24180149
Taraneh Mohebi <taraneh.m5257@gmail.com>
2024-01-11 23:07:24 +0330

    add instagramposts component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import CategoryList from "@/components/CategoryList";
 import { RecipesList } from "@/components/RecipesList";
 import { LearnMore } from "@/components/LearnMore";
+import { InstagramPosts } from "@/components/InstagramPosts";
 export default function Home() {
   return (
-    <div className="max-w-screen-2xl mx-auto">
-      <Navbar />
+    <>
       <Header />
       <CategoryList />
       <RecipesList />
       <LearnMore />
-    </div>
+      <InstagramPosts />
+    </>
   );
 }
---
+++ components/InstagramPosts.tsx
@@ -0,0 +1,51 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+import Posts from "../helper/posts.json";
+import Link from "next/link";
+import Image from "next/image";
+import { FC } from "react";
+interface IInstagramPostsProps {}
+
+export const InstagramPosts: FC<IInstagramPostsProps> = (props) => {
+  return (
+    <Container className="max-w-screen-2xl bg-gradient-to-t from-skyblue">
+      <Grid
+        container
+        sx={{ rowGap:8, justifyContent: "center" }}
+        className="max-w-screen-xl py-28 m-auto"
+      >
+        <div className="flex flex-col justify-center items-center gap-10 ">
+          <h1>Check out @foodieland on Instagram</h1>
+          <Typography paragraph className="w-11/12 ">
+            Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqut enim ad minim
+          </Typography>
+        </div>
+        <Grid container sx={{ rowGap: 4, justifyContent: "center" }}>
+          {Posts.map(({ id, url, image }) => (
+            <Grid
+              item
+              key={id}
+              xs={12}
+              md={6}
+              lg={4}
+              xl={3}
+              className="flex justify-center items-center  "
+            >
+              <Link href={url}>
+                <Image
+                  src={image}
+                  alt="instagram post"
+                  width={290}
+                  height={445}
+                />
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
